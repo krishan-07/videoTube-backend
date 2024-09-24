4c386beaafae068db52de56ab9176a95ebf6bdd3
krishan <krishanmondal273@gmail.com>
2024-09-24 21:24:43 +0530

    add register user and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       requires: true,
       unique: true,
@@ -19,7 +19,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       requires: true,
       trim: true,
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function () {
     {
       _id: this._id,
       email: this.email,
-      username: this.username,
+      userName: this.userName,
       fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer..middleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser);
+userRouter.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImg",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default userRouter;
